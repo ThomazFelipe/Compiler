1c10ef5016b6af032e3d5c65227bf0bfdd70db44
Thomaz Felipe <thomaz.silva@neppoadm.com.br>
2018-06-18 13:45:13 -0300

    updated semantic analysis

--- src/com/unitri/comp/SemanticAnalysis.java
+++ src/com/unitri/comp/SemanticAnalysis.java
@@ -1,6 +1,10 @@
 package com.unitri.comp;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.HeaderTokenizer.Token;
 
 public class SemanticAnalysis {
 
@@ -8,18 +12,64 @@ public class SemanticAnalysis {
 
     private void declaration ( Node node ) {
 
-        String type = type( node.getSon( 1 ) );
+        if ( !Objects.equals( node.getType(), "declaration" ) ) {
+            Log.error( "SemanticAnalysis", "Invalid node in declaration!" );
+            return;
+        }
+        
+        String type = type( node.getSon( 0 ) );
 
-        List< Token > ids = ids( node.getSon( 2 ) );
+        List< Token > ids = ids( node.getSon( 1 ) );
+
+        append( type );
     }
 
     private String type( Node node ) {
+
+        if ( !Objects.equals( node.getType(), "type" ) ) {
+            Log.error( "SemanticAnalysis", "Invalid node in type!" );
+        }
 
         return node.getSon( 0 ).getToken().getImage();
     }
 
     private List< Token > ids( Node node ) {
 
-        return null;
+        if ( !Objects.equals( node.getType(), "ids" ) ) {
+            Log.error( "SemanticAnalysis", "Invalid node in ids!" );
+        }
+
+        List< Token > tokens = new ArrayList<>();
+
+        tokens.add( node.getSon( 0 ).getToken() );
+        tokens.addAll( ids2( node.getSon( 1 ) ) );
+
+        return tokens;
+    }
+
+    private List< Token > ids2( Node node ) {
+
+        if ( !Objects.equals( node.getType(), "ids2" ) ) {
+            Log.error( "SemanticAnalysis", "Invalid node in ids2!" );
+        }
+
+        List< Token > tokens = new ArrayList<>();
+
+        if ( node.getSons().size() > 0 ) {
+
+            tokens.add( node.getSons().get( 0 ).getToken() );
+
+            if ( node.getSons().get( 1 ).getSons().size() > 0 ) {
+
+                tokens.addAll( ids2( node.getSons().get( 1 ) ) );
+            }
+        }
+
+        return tokens;
+    }
+
+    private void append( String text ) {
+
+        code.append( text + " " );
     }
 }
